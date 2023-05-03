f670de203f90d509a3a1f3121af6c30d95713eae
test2 <test2@email.com>
2023-05-04 06:57:40 +0900

    子目標確認関数修正

--- WEB-INF/src/goal/Goal_delete_year.java
+++ WEB-INF/src/goal/Goal_delete_year.java
@@ -16,7 +16,7 @@ public class Goal_delete_year {
         String goal_year_parent_id = dao.check_parent(goal_id);//親目標idを取得
 
         if(goal_year_parent_id != null){//親目標にデータがあるか判定
-            if(dao.check_child(goal_id)){//子目標にデータがあるか判定
+            if(dao.checks_child(goal_id)){//子目標にデータがあるか判定
                 if(del.child_del_all(goal_id) && del.parent_del_all(goal_id) && del.goal_del(goal_id)){//子・親・年間目標削除
                     return true;
                 }else{
